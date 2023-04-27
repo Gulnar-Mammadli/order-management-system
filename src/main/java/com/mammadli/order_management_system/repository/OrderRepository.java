59e805dc2c8d74b1f19a2e89c85f6e856fdd1d58
mammadli <gulnar.mammadli@ut.ee>
2023-04-27 14:51:13 +0300

    Add a custom query to find orders by productId

--- src/main/java/com/mammadli/order_management_system/repository/OrderRepository.java
+++ src/main/java/com/mammadli/order_management_system/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.mammadli.order_management_system.repository;
 
 import com.mammadli.order_management_system.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -13,4 +15,7 @@ public interface OrderRepository extends JpaRepository<Order,Long> {
 
     Optional<Order> findByCustomerId(Long customerId);
     Optional<List<Order>> findAllByDate(LocalDate date);
+
+    @Query("SELECT o FROM Order o JOIN o.orderLines ol WHERE ol.product.id = :productId")
+    List<Order> findOrdersByProductId(@Param("productId") Long productId);
 }
